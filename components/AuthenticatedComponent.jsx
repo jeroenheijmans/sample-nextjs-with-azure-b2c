2152c3e1c2ab06aafd11157200e4f2f0935c19f6
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-07-08 00:42:55 +0200

    Remove unneeded state keeping
    Seems that navigateToLoginRequestUrl defaults to true for MSAL and
    that already has this logic in itself.

--- components/AuthenticatedComponent.jsx
+++ components/AuthenticatedComponent.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {MsalAuthenticationTemplate} from '@azure/msal-react';
 import {InteractionType} from '@azure/msal-browser';
 import {loginRequest} from '../services/authConfig';
-import {useRouter} from 'next/router';
 import NoSsr from './NoSsr';
 
 export function AuthenticatedComponent(props) {
@@ -13,9 +12,6 @@ export function AuthenticatedComponent(props) {
   function ErrorComponent() {
     return <p>Something went wrong...</p>;
   }
-
-  const router = useRouter();
-  loginRequest.state = router.asPath;
 
   return (
     <NoSsr>
