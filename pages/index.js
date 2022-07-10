2c3e3145e7ca17320aa84675de813acd1ccdc93d
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-07-10 17:24:12 +0200

    Tweak overlay setup slightly

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import { useEffect } from "react";
 import Router from "next/router";
 import LoadingOverlay from "../components/LoadingOverlay";
 
-let x = 1;
-
 export default function Index() {
   useEffect(() => {
+    // Delay is here for demonstration purposes, plus it ensures users
+    // don't get a brief flash of the overlay. Some different styling
+    // could solve that more graciously.
     const delay = setTimeout(() => Router.replace("/home", ), 1000);
     return () => clearTimeout(delay);
   });
